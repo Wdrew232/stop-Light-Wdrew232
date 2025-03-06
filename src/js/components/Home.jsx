a73f0f853e7b91aff18b9313035d0d9acf4ff5e8
Wdrew232 <wdrew232@gmail.com>
2025-03-06 03:36:29 +0000

    code updated

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { PureComponent, useState } from "react";
 
 // Include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,35 +6,30 @@ import rigoImage from "../../img/rigo-baby.jpg";
 // Create your first component
 const Home = () => {
     const [color, setColor] = useState("red");
+		
 
     const Click = () => {
         if (color === "red") {
             setColor("yellow");
         } else if (color === "yellow") {
             setColor("green");
-        } else {
-            setColor("red");
-        }
-    };
-
-	const Clicked = () => {
-		if (color === "green"){
-			setColor("purple")
-		}
+        } else if (color === "green") {
+            setColor("purple");
+        } else ( setColor("red"));
 	};
-
     return (
-        <div className="text-center">
+        <div className="pole">
             <div className="trafficLight">
-                <div className="container">
-                    <div className={`circle ${color === "red" ? "red-glow" : ""}`}></div>
-                    <div className={`circle ${color === "yellow" ? "yellow-glow" : ""}`}></div>
-                    <div className={`circle ${color === "green" ? "green-glow" : ""}`}></div>
-					<div className={`circle ${color === "green" ? "purple-glow" : ""}`}></div>
-                </div>
-                <button onClick={Click} onclick={Clicked} className="btn bg-danger">
-					<img src="https://img.icons8.com/?size=60&id=107514&format=png" />
-				</button>
+				<div className="cover"></div>
+					<div className="Lights">
+						<div className={`circle ${color === "red" ? "red-glow" : ""}`}></div>
+						<div className={`circle ${color === "yellow" ? "yellow-glow" : ""}`}></div>
+						<div className={`circle ${color === "green" ? "green-glow" : ""}`}></div>
+						<div className={`circle ${color === "green" ? "purple-glow" : ""}`}></div>
+					</div>
+					<button onClick={Click} className="btn bg-danger">
+						<img src="https://img.icons8.com/?size=60&id=107514&format=png" />
+					</button>
 				
             </div>
         </div>
